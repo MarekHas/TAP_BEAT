3452a70ff9287631709f3e47f420ffd828f568e7
MH <m.hasulak@gmail.com>
2019-03-20 17:53:27 +0100

    View - prototype of SoundtrackView
    Create SoundtrackView script managing view of track based on defined SoundtrackData

---
+++ TAP_BEAT/Assets/Scripts/SoundtrackView.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TapBeat
+{
+    public class SoundtrackView : MonoBehaviour
+    {
+
+        [SerializeField] private SoundtrackData _soundtrackData;
+
+        [SerializeField] private RectTransform _rightArrow;
+        [SerializeField] private RectTransform _leftArrow;
+        [SerializeField] private RectTransform _upArrow;
+        [SerializeField] private RectTransform _downArrow;
+
+        [SerializeField] RectTransform _emptyField;
+
+        RectTransform _rectTransform;
+
+        Vector2 _soundTrackViewPosition; 
+
+		public float SoundtrackViewPosition
+        {
+            get { return _soundTrackViewPosition.y; }
+            set
+            {
+                if (value != _soundTrackViewPosition.y)
+                {
+                    _soundTrackViewPosition.y = value;
+                    _rectTransform.anchoredPosition = _soundTrackViewPosition;
+                }
+            }
+        }
+
+        public void Init(SoundtrackData _soundtrackData)
+        {
+            _rectTransform = (RectTransform)transform;
+
+            _soundTrackViewPosition = _rectTransform.anchoredPosition;
+
+            foreach (int arrowNumber in _soundtrackData.beatsList)
+            {
+                GameObject g;
+                switch (arrowNumber)
+                {
+                    case 0:
+                        g = _rightArrow.gameObject;
+                        break;
+                    case 1:
+                        g = _rightArrow.gameObject;
+                        break;
+                    case 2:
+                        g = _upArrow.gameObject;
+                        break;
+                    case 3:
+                        g = _downArrow.gameObject;
+                        break;
+                    default:
+                        g = _emptyField.gameObject;
+                        break;
+                }
+                Transform view = GameObject.Instantiate(g, transform).transform;
+                view.SetAsFirstSibling();
+
+            }
+        }
+
+        private void Start()
+        {
+            Init(_soundtrackData);
+        }
+
+        void Update()
+        {
+            SoundtrackViewPosition -=  Time.deltaTime * 120;
+        }
+    }
+
+}
