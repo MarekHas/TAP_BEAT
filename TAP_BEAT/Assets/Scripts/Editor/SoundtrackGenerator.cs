15d0ca55258aa849fd28eb86a92387d34f1bcc7a
MH <m.hasulak@gmail.com>
2019-03-20 17:05:55 +0100

    Create Soundtrack Model
    Create ScriptableObject Script > "SoundtrackData", and Editor script > "SoundtrackGenerator" for generate soundtrack with random values of SoudtrackData class attributes

---
+++ TAP_BEAT/Assets/Scripts/Editor/SoundtrackGenerator.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+namespace TapBeat
+{
+    [CustomEditor(typeof(SoundtrackData))]
+    public class SoundtrackGenerator : Editor
+    {
+        private SoundtrackData _soundtrack;
+
+        private Vector2 _attributesScrollViewPosition;
+        private bool _displayData;
+
+        private void OnEnable()
+        {
+            _soundtrack = (SoundtrackData)target;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            if (_soundtrack.beatsList.Count == 0)
+            {
+                EditorGUILayout.HelpBox("Empty soundtrack", MessageType.Info);
+
+                if (GUILayout.Button("Generate soudtrack with randoms", EditorStyles.miniButton))
+                    _soundtrack.GenerateRandomBeats();
+            }
+            else
+            {
+                if (GUILayout.Button("Update soudtrack attributes", EditorStyles.miniButton))
+                    _soundtrack.GenerateRandomBeats();
+
+                _displayData = EditorGUILayout.Foldout(_displayData, "Display Beats");
+
+                if (_displayData)
+                {
+                    _attributesScrollViewPosition = EditorGUILayout.BeginScrollView(_attributesScrollViewPosition);
+
+                    for (int i = 0; i < _soundtrack.beatsList.Count; i++)
+                    {
+                        _soundtrack.beatsList[i] = EditorGUILayout.IntSlider(_soundtrack.beatsList[i], -1, SoundtrackData.inputs - 1);
+                    }
+
+                    EditorGUILayout.EndScrollView();
+                }
+            }
+
+            EditorUtility.SetDirty(_soundtrack);
+        }
+    }
+}
+
---
+++ TAP_BEAT/Assets/Scripts/SoundtrackData.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TapBeat
+{
+    [CreateAssetMenu(menuName = "TAP_BEAT/New Soundtrack", fileName = "New Soundtrack.asset")]
+    public class SoundtrackData : ScriptableObject
+    {
+
+        [Range(40, 200)] public int bpm = 120;
+
+        [HideInInspector] public List<int> beatsList = new List<int>();
+
+#if UNITY_EDITOR 
+       
+        static public int inputs = 4;
+        [Range(0f, 10f)] [SerializeField] private int _entry = 10;
+        [Range(1, 20)] [SerializeField] private int _minSectionSize= 2;
+        [Range(1, 20)] [SerializeField] private int _maxSectionSize = 5;
+        [Range(1, 20)] [SerializeField] private int _minBreakSize = 1;
+        [Range(1, 20)] [SerializeField] private int _maxBreakSize = 2;
+        [Range(1, 20)] [SerializeField] private int _sectionsNumber = 10;
+
+        public void GenerateRandomBeats()
+        {
+            beatsList = new List<int>();
+           
+            for (int i = 0; i < _entry; i++)
+            {
+                beatsList.Add(-1);
+            }
+
+            for (int i = 0; i < _sectionsNumber; i++)
+            {
+                int blockLength = Random.Range(_minSectionSize, _maxSectionSize + 1);
+                for (int b = 0; b < blockLength; b++)
+                {
+                    int beat = Random.Range(0, inputs);
+                    beatsList.Add(beat);
+                }
+                //dont add empty blocks at the end of all sections
+                if (i == _sectionsNumber - 1)
+                    break;
+
+                int breakLength = Random.Range(_minBreakSize, _maxBreakSize + 1);
+                for (int b = 0; b < breakLength; b++)
+                {
+                    beatsList.Add(-1);
+                }
+            }
+        }
+#endif
+    }
+}
+
